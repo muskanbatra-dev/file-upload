6bbafea3d47b20e2851de3b2935856ad888116d3
muskanbatra-dev <95659002+muskanbatra-dev@users.noreply.github.com>
2025-12-18 03:53:34 +0530

    title: login page and cors error fixed

--- apps/backend/index.js
+++ apps/backend/index.js
@@ -1,16 +1,37 @@
-import dotenv from "dotenv";
-dotenv.config();
+import express from "express";
 import cors from "cors";
-
-import app from "./app.js";
+import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+import authRoutes from "./routes/authe.route.js";
+import fileRoutes from "./routes/file.route.js";
+dotenv.config();
+
+const app = express();
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    const allowedOrigins = [
+      "http://localhost:5174",
+      "https://muskanbatra-fileupload.vercel.app",
+    ];
+
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.use(express.json());
+app.use(cors(corsOptions));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/files", fileRoutes);
+
 const PORT = process.env.PORT || 5000;
 
 connectDB();
--- apps/web/src/pages/LoginPage.jsx
+++ apps/web/src/pages/LoginPage.jsx
@@ -1,26 +1,45 @@
-
 import { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   const handleLogin = async () => {
-    const res = await axios.post("/auth/login", { email, password });
-    localStorage.setItem("token", res.data.token);
-    window.location.href = "/";
+    try {
+      const res = await axios.post(
+        "https://file-upload-pry8.onrender.com/api/auth/login",
+        { email, password }
+      );
+
+      localStorage.setItem("token", res.data.token);
+
+      
+      navigate("/", { replace: true });
+    } catch (err) {
+      alert(err.response?.data?.message || "Login failed");
+    }
   };
 
   return (
     <div>
       <h2>Login</h2>
-      <input placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+
+      <input
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
       <input
         type="password"
         placeholder="Password"
+        value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
+
       <button onClick={handleLogin}>Login</button>
     </div>
   );
